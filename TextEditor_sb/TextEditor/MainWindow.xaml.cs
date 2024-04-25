ccb0e1ce380e972e6b350066a26f93229bfac523
Batyr Daurov <118624947+BatyrDaurov@users.noreply.github.com>
2024-04-25 13:49:05 +0300

    Add files via upload

---
+++ TextEditor_sb/TextEditor/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using Microsoft.Win32;
+using System.Windows.Controls;
+
+namespace WpfTutorialSamples.Rich_text_controls
+{
+    public partial class RichTextEditorSample : Window
+    {
+        public RichTextEditorSample()
+        {
+            InitializeComponent();
+            cmbFontFamily.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+            cmbFontSize.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+        }
+
+        private void MenuItem_Exit(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show(
+             "Закрыть?",
+             "Выход",
+             MessageBoxButton.YesNo,
+             MessageBoxImage.Stop);
+
+            if (result == MessageBoxResult.Yes)
+                Close();
+        }
+
+        private void MenuItem_New(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void MenuItem_Open(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
+            {
+                FileStream fileStream = new FileStream(dlg.FileName, FileMode.Open);
+                TextRange range = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+                range.Load(fileStream, DataFormats.Rtf);
+            }
+        }
+
+        private void MenuItem_Save(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
+            {
+                FileStream fileStream = new FileStream(dlg.FileName, FileMode.Create);
+                TextRange range = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+                range.Save(fileStream, DataFormats.Rtf);
+            }
+
+        }
+
+        private void MenuItem_SaveAs(object sender, RoutedEventArgs e)
+        { }
+
+        private void MenuItem_Cut(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(rtbEditor.Selection.Text);
+            rtbEditor.Selection.Text = "";
+        }
+
+        private void MenuItem_Copy(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(rtbEditor.Selection.Text);
+        }
+
+        private void MenuItem_Paste(object sender, RoutedEventArgs e)
+        {
+            rtbEditor.Selection.Text += Clipboard.GetText();
+        }
+
+        private void MenuItem_Cancel(object sender, RoutedEventArgs e)
+        {
+            rtbEditor.Undo();
+        }
+
+
+        private void rtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            object temp = rtbEditor.Selection.GetPropertyValue(Inline.FontWeightProperty);
+            btnBold.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontWeights.Bold));
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontStyleProperty);
+            btnItalic.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontStyles.Italic));
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+            btnUnderline.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(TextDecorations.Underline));
+
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontFamilyProperty);
+            cmbFontFamily.SelectedItem = temp;
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontSizeProperty);
+            cmbFontSize.Text = temp.ToString();
+        }
+
+        private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            
+        }
+
+        private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            
+        }
+
+        private void cmbFontFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbFontFamily.SelectedItem != null)
+                rtbEditor.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, cmbFontFamily.SelectedItem);
+        }
+
+        private void cmbFontSize_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            rtbEditor.Selection.ApplyPropertyValue(Inline.FontSizeProperty, cmbFontSize.Text);
+        }
+    }
+}
